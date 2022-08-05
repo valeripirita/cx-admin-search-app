0c99aa4fc24d23dbc5b0a8c760710df02265645e
igor.zavorotnyi <igor.zavorotnyi@gamesys.com>
2022-08-05 10:34:36 +0300

    GTECH-702606-search-app
    bff search improvements

--- cx-admin-search-app-service/service/src/main/java/gamesys/csp/searchbffservice/controller/SearchController.java
+++ cx-admin-search-app-service/service/src/main/java/gamesys/csp/searchbffservice/controller/SearchController.java
@@ -1,6 +1,9 @@
 package gamesys.csp.searchbffservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import gamesys.csp.searchbffservice.configuration.SearchServiceProperties;
+import gamesys.csp.searchbffservice.model.SearchAttributes;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -8,7 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
@@ -28,15 +33,30 @@ public class SearchController {
     }
 
     @PostMapping(path = "/search")
-    public ResponseEntity<String> response() {
-
-        HttpHeaders headers = new HttpHeaders();
+    public String search(@RequestBody SearchAttributes searchAttributes) {
+        log.info("Search is started");
+        var headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Api-Key", properties.getApiKey());
 
-        String url = properties.getAccountServiceUrl();
-        HttpEntity requestEntity = new HttpEntity(null, headers);
+        try {
+            var body = getBody(searchAttributes);
+            var requestEntity = new HttpEntity(body, headers);
+            var response = restTemplate.postForEntity(properties.getAccountServiceUrl() + "/search", requestEntity, String.class);
+            log.info("Search is completed");
+            return response.getBody();
+        } catch (JsonProcessingException ex) {
+            log.error("Json serialization problem: " + ex.getMessage());
+            return "";
+        }
+        catch (RestClientException ex){
+            log.error("Issue during post request: " + ex.getMessage());
+            return "";
+        }
+    }
 
-        return restTemplate.postForEntity(url + "/search", requestEntity, String.class);
+    private String getBody(SearchAttributes attributes) throws JsonProcessingException {
+        var mapper = new ObjectMapper();
+        return mapper.writeValueAsString(attributes);
     }
 }
