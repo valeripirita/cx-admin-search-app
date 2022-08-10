7d62fc65fd576915d4ae80db34f98ae1566dfc02
artjom.sakirov <artjom.sakirov@gamesys.com>
2022-08-10 15:17:48 +0300

    GTECH-702606 update bdd tests

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -34,7 +34,7 @@ async function setupNodeEvents(
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:8083',
+        baseUrl: 'http://192.168.1.5:8083/',
         specPattern: '**/*.feature',
         supportFile: false,
         video: false,
