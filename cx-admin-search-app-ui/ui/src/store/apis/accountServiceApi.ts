3bb8dbb371d4f4c162821cbb60ec71a57fc8e98b
andrei.samborskii <andrei.samborskii@gamesys.co.uk>
2022-08-16 15:10:32 +0300

    GTECH-702606 modify data mapping on ui

--- cx-admin-search-app-ui/ui/src/store/apis/accountServiceApi.ts
+++ cx-admin-search-app-ui/ui/src/store/apis/accountServiceApi.ts
@@ -1,80 +1,25 @@
 import { baseApi } from './baseApi';
 
-export type Link = {
-    href: string;
-}
-
-export type LinksList = {
-    self: Link;
-    first?: Link;
-    next?: Link;
-    last?: Link;
-}
-
-export type AccountPersonalDetails = {
-    dateOfBirth: string;
-    title: string;
-    firstName: string;
-    surname: string;
-}
-
-export type ContactNumber = {
-    number: string;
-    verifiedStatus: string;
-}
-
-export type AccountContactNumbers = {
-    _links: LinksList;
-    home: ContactNumber;
-    mobile: ContactNumber;
-    work: ContactNumber;
-}
-
-export type AccountResidentialAddress = {
-    _links: LinksList,
-    addressLine1: string;
-    addressLine2: string;
-    addressLine3: string;
-    addressLine4: string;
-    countryCode: string;
-    postCode: string;
-}
-
-export type SubscriptionPreference = {
-    type: string;
-    status: string;
-}
-
-export type AccountSubscriptionPreferences = {
-    _links: LinksList;
-    subscriptionPreferences: SubscriptionPreference[];
-}
-
 export type Account = {
-    accountId: number;
-    acquisitionPartnerId: number;
-    currencyIso: string;
-    emailAddress: string;
-    emailVerificationStatus: string;
-    open: boolean;
-    personalDetails: AccountPersonalDetails;
-    screenName: string;
-    venture: string;
-    _embedded: {
-        contactNumbers: AccountContactNumbers[];
-        residentialAddress: AccountResidentialAddress[];
-        subscriptionPreferences: AccountSubscriptionPreferences[];
-    }
+    accountId: number
+    addressLine2: string
+    countryCode: string
+    dateOfBirth: string
+    emailAddress: string
+    firstName: string
+    number: string
+    open: boolean
+    postCode: string
+    screenName: string
+    surname: string
+    title: string
+    venture: string
 }
 
 export type AccountServiceResponse = {
-    _links: LinksList;
-    limit: number,
-    offset: number,
+    accountDtos: Account[],
     totalResults: number,
-    _embedded: {
-        accounts: Account[]
-    }
+    offset: number,
 }
 
 export type AccountServicePostObject = {
@@ -98,15 +43,12 @@ export const accountServiceApi = baseApi('accountServiceApi', []).injectEndpoint
             query: data => ({
                 url: '/search',
                 method: 'POST',
-                body: {
-                    ...data
-                }
+                body: { ...data }
             })
         }),
         fetchAccountVentures: build.query<AccountVenturesResponse, any>({
             query: (userId) => ({
-                url: `/ventures?userId=${userId}`,
-                method: 'GET',
+                url: `/ventures?userId=${userId}`
             })
         })
     })
